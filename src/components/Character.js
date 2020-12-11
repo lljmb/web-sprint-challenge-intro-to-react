fca71f86159902c3fe2166be2ffd056381e4f43b
lljmb <brown.jessm@icloud.com>
2020-12-11 13:04:18 -0500

    completed mvp for sprint

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,30 @@
 // Write your Character component here
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import styled from 'styled-components';
+
+export default function Character (props){ 
+    const { character } = props;
+
+    return ( 
+        <div className='position relative translate-middle'>
+<div className="row row-cols-4 row-cols-md-2 g-2">
+  <div className="col">
+    <div className="card">
+      <div className="card-body">
+        <h5 className ="card-title">Name: {character.name}</h5>
+        <p className='card-text'>Gender: {character.gender === 'n/a' ? 'droid' : character
+         .gender}</p>
+        <p className='card-text'> Birth Year: {character.birth_year}</p>
+        <p className='card-text'> Height: {character.height}</p>
+        <p className='card-text'> Mass: {character.mass}</p>
+      </div>
+    </div>
+  </div>
+  </div>
+  </div>
+    )
+
+
+}
+
